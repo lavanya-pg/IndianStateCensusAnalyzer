b8f8b2a85b2424d1953308d6a7ca587fb017b1e0
lavanya-pg <lavanyapg8@gmail.com>
2021-07-26 21:09:21 +0530

    ADDED: Ability to Load the Indian State Census Information From CSV File

---
+++ lib/src/main/java/indianstateanalyzer/CensusAnalyserException.java
@@ -0,0 +1,26 @@
+package indianstateanalyzer;
+
+public class CensusAnalyserException extends Exception
+{
+
+    enum ExceptionType
+    {
+        CENSUS_FILE_PROBLEM,
+        INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type)
+    {
+        super(message);
+        this.type = type;
+    }
+
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) 
+    {
+        super(message, cause);
+        this.type = type;
+    }
+}
