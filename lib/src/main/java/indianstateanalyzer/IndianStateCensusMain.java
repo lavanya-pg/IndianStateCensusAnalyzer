3f150f0907d36a00ae680a4dc11fbf3b1eee96b2
lavanya-pg <lavanyapg8@gmail.com>
2021-07-26 15:13:13 +0530

    Initial Commit: Welcome To IndianStateCensusAnalyzer Program

---
+++ lib/src/main/java/indianstateanalyzer/IndianStateCensusMain.java
@@ -0,0 +1,10 @@
+package indianstateanalyzer;
+
+public class IndianStateCensusMain 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To the Indian State Census Analyzer Program");
+	}
+
+}
